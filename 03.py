b5717ab6058b7f3ddf7cc10470c4ad72485f70a6
Maja Trepsic <maja.trepsic@gmail.com>
2017-12-04 20:44:02 +0100

    Day 3 part 2

--- 03.py
+++ 03.py
@@ -1,44 +1,48 @@
-import math
-inputs = [10, 11, 19, 23, 31, 32, 1024, 361527]
-#inputs = [1, 12, 23, 1024]
-#n = 19
-
-#1 - 0
-# 10 -3
-# 11 - 2
-#12 -3
-#23 - 2
-# 31
-# 32 - 5
-#1024 - 31
-
-for n in inputs:
-    print 'n: ' + str(n)
-    next_root = math.ceil(math.sqrt(n))
-    next_odd_root = 0
-    if next_odd_root%2 != 0:
-        next_odd_root = next_root
+def find_next_index(i, j, square):
+    if (j+1 < len(square)) and (square[i][j+1] == 0):
+        j += 1
+        # print 'right'
+    elif (i-1 >= 0) and (square[i-1][j] == 0):
+        i -= 1
+        # print 'up'
+    elif (j-1 >= 0) and (square[i][j-1] == 0):
+        j -= 1
+        # print 'left'
     else:
-        next_odd_root = next_root +1
-    print 'next odd root: ' + str(next_odd_root)
+        i += 1
+        #  print 'down'
+    return i, j
 
 
-    last_in_square = next_odd_root*next_odd_root
-    print 'last_in_square: ' + str(last_in_square)
-    distance_from_last_in_square = last_in_square-n
-    print 'distance_from_last_in_square: ' + str(distance_from_last_in_square)
+def sum_of_adjacent_cells(i, j, square):
+    adjacent_cells = square[max(i-1,0):min(i+1,len(square)-1)+1, max(j-1,0):min(j+1,len(square)-1)+1]
+    # print adjacent_cells
+    return adjacent_cells.sum()
 
-    min_circle_value = math.floor(next_odd_root/2)
-    print 'min_circle_value: ' + str(min_circle_value)
 
-    distance_from_corner = distance_from_last_in_square % (next_odd_root-1)
-    print 'distance_from_corner: ' + str(distance_from_corner)
+input = 361527
+k = 1
+i = j = 0
+current_value = 1
+square_size = 1
+square = [[current_value]]
 
-    if distance_from_corner > next_odd_root/2:
-        distance_from_corner = next_odd_root-1 - distance_from_corner
-        print 'distance_from_corner: ' + str(distance_from_corner)
+import numpy as np
 
-    solution = next_odd_root-1-distance_from_corner
+while input >= current_value:
+    k += 1
+    if k > square_size*square_size:
+        # increase matrix
+        square_size += 2
+        old_square = square
+        square = np.zeros(shape=(square_size, square_size),  dtype=np.int)
+        square[1:square_size-1,1:square_size-1] = old_square
+        j += 1
+        i += 1
+    i, j = find_next_index(i, j, square)
+    square[i][j] = sum_of_adjacent_cells(i, j, square)
+    current_value = square[i][j]
+    # print current_value
+    # print square
 
-    print 'solution: ' + str(solution)
-    print '------------------------'
+print current_value
